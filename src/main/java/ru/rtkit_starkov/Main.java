ee190cd82760f94f9a617de0f64a255728421b51
nikita.starkov <nikita.starkov@south.rt.ru>
2024-03-01 18:58:49 +0500

    Happy world commit

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -3,6 +3,6 @@ package ru.rtkit_starkov;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Semen krasavchik");
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!!!");
     }
 }

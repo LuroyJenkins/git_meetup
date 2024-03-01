d928c6a9dae5660a7905bffd4625b41cd9173496
nikita.starkov <nikita.starkov@south.rt.ru>
2024-03-01 19:02:35 +0500

    Add Nikitos

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Semen krasavchik");
         System.out.println("Hello world!!!!!!!");
+
+        System.out.println("Nikitos QA forever");
     }
 }

1c4c9a502374fd144e071ef094e00d7a39bd82eb
nikita.starkov <nikita.starkov@south.rt.ru>
2024-03-01 18:54:09 +0500

    Add Semen krasava

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -2,7 +2,7 @@ package ru.rtkit_starkov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Semen");
+        System.out.printf("Semen krasavchik");
         System.out.println("Hello world!");
     }
 }

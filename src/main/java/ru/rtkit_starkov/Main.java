9588f0f88a5199eb9e6904787696b76c24ec278a
Nikita Starkov <99817031+LuroyJenkins@users.noreply.github.com>
2024-03-01 19:05:08 +0500

    Update Main.java

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Semen");
         System.out.println("Ne hello world!");
+
+        System.out.println("Vasya krassava!");
     }
 }

cf9b856a8f9baa040d737380d37bc47a6a5a75c8
nikita.starkov <nikita.starkov@south.rt.ru>
2024-03-01 18:44:19 +0500

    Add Semen phrase

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -2,6 +2,7 @@ package ru.rtkit_starkov;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("Semen");
         System.out.println("Hello world!");
     }
 }

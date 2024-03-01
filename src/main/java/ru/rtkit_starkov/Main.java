6bd146c82233dbb72f8447888a768e384b32ccc5
Nikita Starkov <99817031+LuroyJenkins@users.noreply.github.com>
2024-03-01 18:58:01 +0500

    Update Main.java

--- src/main/java/ru/rtkit_starkov/Main.java
+++ src/main/java/ru/rtkit_starkov/Main.java
@@ -3,6 +3,6 @@ package ru.rtkit_starkov;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Semen");
-        System.out.println("Hello world!");
+        System.out.println("Ne hello world!");
     }
 }
